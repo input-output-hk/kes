f54555153049641cc2280637c0907edefc6270e3
iquerejeta <querejeta.azurmendi@iohk.io>
2021-10-12 12:52:52 +0200

    Binary files and updated key signature

--- kes-mmm-sumed25519/tests/interoperability.rs
+++ kes-mmm-sumed25519/tests/interoperability.rs
@@ -1,25 +1,35 @@
 //! Tests to check for interoperability with Haskell function. To generate the
-//! test data, we ran the following script using the `cardano-base` implementation.
-//!
-//! To generate the data, available in ./data, we ran the following haskell script
-//! for different depths, in particular depth 1 and depth 6.
+//! test data, available in ./data, we ran the following script using the `cardano-base` implementation.
 //!
 //! ```haskell
 //! import Cardano.Crypto.Seed
 //! import Cardano.Crypto.DSIGN
 //! import Cardano.Crypto.Hash
-//! import Cardano.Crypto.KES(Sum1KES, genKeyKES, rawSerialiseSignKeyKES)
+//! import Cardano.Crypto.KES
 //!
+//! import Data.Maybe (fromJust)
 //! import qualified Data.ByteString.Char8 as Bytechar
 //! import qualified Data.ByteString as B
 //!
 //!
 //! main :: IO()
 //! main = let
-//!   seed = mkSeedFromBytes $ Bytechar.pack "test string of 32 byte of lenght"
-//!   kesInstance =  genKeyKES @(Sum1KES Ed25519DSIGN Blake2b_256) seed
-//!   in do
-//!     B.writeFile "/Users/queremendi/iohk/CryptoLibs/kes-mmm-sumed25519/kes-mmm-sumed25519/tests/key1.bin" (rawSerialiseSignKeyKES kesInstance)
+//!     seed = mkSeedFromBytes $ Bytechar.pack "test string of 32 byte of lenght"
+//!     kesSk =  genKeyKES @(Sum6KES Ed25519DSIGN Blake2b_256) seed
+//!     kesSignature = signKES () 0 (Bytechar.pack "test message") kesSk
+//!     kesSkOneUpdate = fromJust (updateKES () kesSk 0)
+//!     kesSkTwoUpdate = fromJust (updateKES () kesSkOneUpdate 1)
+//!     kesSkThreeUpdate = fromJust (updateKES () kesSkTwoUpdate 2)
+//!     kesSkFourUpdate = fromJust (updateKES () kesSkThreeUpdate 3)
+//!     kesSkFiveUpdate = fromJust (updateKES () kesSkFourUpdate 4)
+//!     kesSignatureFive = signKES () 5 (Bytechar.pack "test message") kesSkFiveUpdate
+//!     
+//!     in do
+//!         B.writeFile "<PATH>/key6.bin" (rawSerialiseSignKeyKES kesSk)
+//!         B.writeFile "<PATH>/key6Sig.bin" (rawSerialiseSigKES kesSignature)
+//!         B.writeFile "<PATH>/key6update1.bin" (rawSerialiseSignKeyKES kesSkOneUpdate)
+//!         B.writeFile "<PATH>/key6update5.bin" (rawSerialiseSignKeyKES kesSkFiveUpdate)
+//!         B.writeFile "<PATH>/key6Sig5.bin" (rawSerialiseSigKES kesSignatureFive)
 //! ```
 //!
 use kes_mmm_sumed25519::erasable_buffer::*;
@@ -68,6 +78,25 @@ fn haskell_signature_6() {
     let message = b"test message";
     let (skey, _) = Sum6Kes::keygen(&mut seed.to_owned());
     let signature = skey.sign(0, message);
+    assert_eq!(&signature.to_bytes(), h_signature);
+}
+
+#[test]
+fn haskell_signature_6_update_5() {
+    let h_key: &[u8; 608] = include_bytes!("data/key6update5.bin");
+    let h_signature: &[u8; 448] = include_bytes!("data/key6Sig5.bin");
+
+    let seed = b"test string of 32 byte of lenght";
+    let message = b"test message";
+    let (mut skey, _) = Sum6Kes::keygen(&mut seed.to_owned());
+    skey.update(0).unwrap();
+    skey.update(1).unwrap();
+    skey.update(2).unwrap();
+    skey.update(3).unwrap();
+    skey.update(4).unwrap();
+    assert_eq!(skey.as_bytes(), h_key);
+
+    let signature = skey.sign(5, message);
     assert_eq!(&signature.to_bytes(), h_signature);
 }
 
