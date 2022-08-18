2301ccb2db928504bdd12e909fa6fc627d51a70c
iquerejeta <querejeta.azurmendi@iohk.io>
2022-08-18 11:26:08 +0100

    Minor pass to docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,12 @@
 //! <https://eprint.iacr.org/2001/034>
 //!
 //! Specfically we do the binary sum composition directly as in the paper, and
-//! then use that in a nested\/recursive fashion to construct a 7-level deep
-//! binary tree version.
+//! then use that in a nested\/recursive fashion to construct up to a 7-level
+//! deep binary tree version.
 //!
-//! This relies on "Cardano.Crypto.KES.CompactSingle" for the base case.
-//!
-//! Compared to the implementation in 'Cardano.Crypto.KES.Sum', this flavor
-//! stores only one VerKey in the branch node.
+//! We provide two different implementations in this crate, to provide compatibility
+//! with Cardano's different eras. The first, `SumKes`, is a trivial construction,
+//! while the second, `SumCompactKes`, is a version with a more compact signature.
 //!
 //! Consider the following Merkle tree:
 //!
@@ -27,22 +26,21 @@
 //!
 //! The caret points at leaf node E, indicating that the current period is 1.
 //! The signatures for leaf nodes D through G all contain their respective
-//! DSIGN keys; the signature for branch node B however only holds the signature
-//! for node E, and the VerKey for node D. It can reconstruct its own VerKey
-//! from these two. The signature for branch node A (the root node), then, only
-//! contains the VerKey for node C, and the signature for node B. In other
-//! words, the number of individual hashes to be stored equals the depth of the
-//! Merkle tree. Compare that to the older, naive 'SumKES', where each branch
-//! node stores two VerKeys: here, the number of keys to store is the depth of
-//! the tree times two.
+//! DSIGN keys.
 //!
-//! Note that when we verify such a signature, we need to also compare the
-//! ultimate VerKey at the root against the one passed in externally, because
-//! all VerKeys until that point have been derived from the (user-supplied, so
-//! untrusted) signature. But we only need to do this once, at the tree root,
-//! so we split up the verification into two parts: verifying a signature
-//! against its embedded VerKey, and comparing that VerKey against the
-//! externally supplied target key.
+//! In the naive `SumKes` signatures the signature for branch node B holds
+//! the signature for node E, and the VerKeys for nodes D and E. The signature
+//! for branch node A (the root node), the signature for node B and the
+//! VerKeys for nodes B and C. In other words, the number of individual hashes
+//! to be stored equals the depth of the Merkle tree.
+//!
+//! Instead, the more efficient `SumCompactKes` gets rid of some redundant data
+//! of the signature. In particular, the signature for branch node B only holds
+//! the signature for node E, and the VerKey for node D. It can reconstruct its
+//! own VerKey from these two. The signature for branch node A (the root node),
+//! then, only contains the VerKey for node C, and the signature for node B. In
+//! other words, the number of individual hashes to be stored equals the depth
+//! of the Merkle tree.
 
 mod common;
 #[macro_use]
