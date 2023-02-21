914017002e8552dc39591628f82b440e1d989a4e
iquerejeta <querejeta.azurmendi@iohk.io>
2023-02-21 14:36:31 +0100

    Benches failing

--- benches/mmm-sumed25519.rs
+++ benches/mmm-sumed25519.rs
@@ -1,86 +1,59 @@
 #[macro_use]
 extern crate criterion;
-use criterion::{BatchSize, Criterion};
+use criterion::Criterion;
 use kes_summed_ed25519::kes::{
     Sum1CompactKes, Sum2CompactKes, Sum3CompactKes, Sum4CompactKes, Sum5CompactKes, Sum6CompactKes,
     Sum7CompactKes,
 };
 use kes_summed_ed25519::traits::{KesCompactSig, KesSk};
 
-fn bench_keygen<KES: KesSk>(depth: usize, c: &mut Criterion) {
-    let mut seed = [0u8; 32];
-    c.bench_function(format!("KeyGen with depth: {}", depth).as_str(), |b| {
-        b.iter(|| {
-            KES::keygen(&mut seed);
-        })
-    });
+// Implementing benches with macros, because the closure of benched function creates problems with lifetime of KES
+macro_rules! bench_keygen {
+    ($name:ident, $kes:ident, $depth:expr) => {
+        fn $name(c: &mut Criterion) {
+            c.bench_function(format!("KeyGen with depth: {}", $depth).as_str(), |b| {
+                b.iter(|| {
+                    let mut seed = [0u8; 32];
+                    let mut key_buffer = [0u8; $kes::SIZE + 4];
+                    $kes::keygen(&mut key_buffer, &mut seed);
+                })
+            });
+        }
+    };
 }
 
-fn update_with_depth<KES: KesSk>(depth: usize, nb_update: usize, c: &mut Criterion) {
-    let mut seed = [0u8; 32];
-
-    c.bench_function(format!("Update with depth: {}", depth).as_str(), move |b| {
-        b.iter_batched_ref(
-            || -> KES {
-                let (sk_orig, _) = KES::keygen(&mut seed);
-                sk_orig
-            },
-            |sk_orig| {
-                for _ in 0..(nb_update - 1) {
-                    sk_orig.update().unwrap()
-                }
-            },
-            BatchSize::SmallInput,
-        )
-    });
-}
-
-fn update_with_depth_skip<KES: KesSk>(depth: usize, nb_update_to_skip: usize, c: &mut Criterion) {
-    let mut seed = [0u8; 32];
-    c.bench_function(
-        format!(
-            "Update with depth: {}, and nb_update_to_skip: {}",
-            depth, nb_update_to_skip
-        )
-        .as_str(),
-        |b| {
-            b.iter_batched_ref(
-                || -> KES {
-                    let (mut sk_orig, _) = KES::keygen(&mut seed);
-                    for _ in 0..(nb_update_to_skip - 1) {
-                        sk_orig.update().unwrap()
-                    }
-                    sk_orig
+macro_rules! update_with_depth {
+    ($name:ident, $kes:ident, $depth:expr, $nb_update:expr) => {
+        fn $name(c: &mut Criterion) {
+            c.bench_function(
+                format!("KeyGen and Update with depth: {}", $depth).as_str(),
+                move |b| {
+                    b.iter(|| {
+                        let mut seed = [0u8; 32];
+                        let mut key_buffer = [0u8; $kes::SIZE + 4];
+                        let (mut sk_orig, _) = $kes::keygen(&mut key_buffer, &mut seed);
+                        for _ in 0..($nb_update - 1) {
+                            sk_orig.update().unwrap();
+                        }
+                    })
                 },
-                |sk_orig| sk_orig.update().unwrap(),
-                BatchSize::SmallInput,
-            )
-        },
-    );
+            );
+        }
+    };
 }
 
-fn keygen_depth1(c: &mut Criterion) {
-    bench_keygen::<Sum1CompactKes>(1, c)
-}
-fn keygen_depth2(c: &mut Criterion) {
-    bench_keygen::<Sum2CompactKes>(2, c)
-}
-fn keygen_depth3(c: &mut Criterion) {
-    bench_keygen::<Sum3CompactKes>(3, c)
-}
-fn keygen_depth4(c: &mut Criterion) {
-    bench_keygen::<Sum4CompactKes>(4, c)
-}
-fn keygen_depth6(c: &mut Criterion) {
-    bench_keygen::<Sum6CompactKes>(6, c)
-}
-fn keygen_depth7(c: &mut Criterion) {
-    bench_keygen::<Sum7CompactKes>(7, c)
-}
+bench_keygen!(keygen_depth1, Sum1CompactKes, 1);
+bench_keygen!(keygen_depth2, Sum2CompactKes, 2);
+bench_keygen!(keygen_depth3, Sum3CompactKes, 3);
+bench_keygen!(keygen_depth4, Sum4CompactKes, 4);
+bench_keygen!(keygen_depth6, Sum6CompactKes, 6);
+bench_keygen!(keygen_depth7, Sum7CompactKes, 7);
 
 fn sign_depth5(c: &mut Criterion) {
     let mut seed = [0u8; 32];
-    let (sk, _) = Sum5CompactKes::keygen(&mut seed);
+    let mut key_buffer = [0u8; Sum5CompactKes::SIZE + 4];
+
+    let (sk, _) = Sum5CompactKes::keygen(&mut key_buffer, &mut seed);
     let msg = [0u8; 256];
     c.bench_function("Signature with depth 5", |b| {
         b.iter(|| {
@@ -91,7 +64,8 @@ fn sign_depth5(c: &mut Criterion) {
 
 fn verify_depth7(c: &mut Criterion) {
     let mut seed = [0u8; 32];
-    let (sk, pk) = Sum7CompactKes::keygen(&mut seed);
+    let mut key_buffer = [0u8; Sum7CompactKes::SIZE + 4];
+    let (sk, pk) = Sum7CompactKes::keygen(&mut key_buffer, &mut seed);
     let msg = [0u8; 256];
     let signature = sk.sign(&msg);
     c.bench_function("Signature verification with depth 7", |b| {
@@ -101,19 +75,9 @@ fn verify_depth7(c: &mut Criterion) {
     });
 }
 
-fn update2_depth2(c: &mut Criterion) {
-    update_with_depth::<Sum2CompactKes>(2, 2, c)
-}
-fn update4_depth4(c: &mut Criterion) {
-    update_with_depth::<Sum4CompactKes>(4, 4, c)
-}
-fn update16_depth7(c: &mut Criterion) {
-    update_with_depth::<Sum7CompactKes>(7, 16, c)
-}
-
-fn update128_depth7(c: &mut Criterion) {
-    update_with_depth_skip::<Sum7CompactKes>(7, (1 << 7) - 1, c)
-}
+update_with_depth!(update2_depth2, Sum2CompactKes, 2, 2);
+update_with_depth!(update4_depth4, Sum4CompactKes, 4, 4);
+update_with_depth!(update16_depth7, Sum7CompactKes, 7, 16);
 
 criterion_group!(
     keygen_benches,
@@ -132,7 +96,6 @@ criterion_group!(
     update2_depth2,
     update4_depth4,
     update16_depth7,
-    update128_depth7
 );
 
 criterion_main!(keygen_benches, keyopts_benches);
